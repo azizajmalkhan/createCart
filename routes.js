0142d42bfb5bc104d03070b220e8dc764bfe44bb
azizajmalkhan <126547245+azizajmalkhan@users.noreply.github.com>
2025-01-26 21:14:47 +0530

    routes.js

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const cartController = require("./cartController");  // Import the controller
 const loginController=require("./loginController");
+const createProduct=require("./createProductController");
 
 // Define routes for variants and modifiers
 // router.get("/variants", cartController.createCart);
 //router.get("/modifiers", cartController.modifiers);
 router.post("/variants",loginController.verifyUser, cartController.createCart);
 router.post("/api/login",loginController.loginApi)
+router.post("/createProduct",createProduct.createProduct)
 // Export the router
 module.exports = router;
