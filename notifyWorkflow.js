55b7b760a50e3473de28a271ad576935275858b7
azizajmalkhan <126547245+azizajmalkhan@users.noreply.github.com>
2025-04-06 20:25:23 +0530

    Update notifyWorkflow.js

--- notifyWorkflow.js
+++ notifyWorkflow.js
@@ -1,16 +1,46 @@
 const nodemailer = require('nodemailer');
-//const twilio = require('twilio');
+// //const twilio = require('twilio');
 
-// MySQL database connection
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'yourpassword',
-//   database: 'your_database'
-// });
+// // MySQL database connection
+// // const connection = mysql.createConnection({
+// //   host: 'localhost',
+// //   user: 'root',
+// //   password: 'yourpassword',
+// //   database: 'your_database'
+// // });
 
-// Send email function
-function sendEmail(order) {
+// // Send email function
+// let sendEmail = function sendEmail(req,res) {
+//   const order = req.body.order_id;
+//   let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: '24eg204a18@anurag.edu.in',
+//       pass: 'au@123456'
+//     }
+//   });
+
+//   let mailOptions = {
+//     from: '24eg204a18@anurag.edu.in',
+//     to: 'vorebe3794@noroasis.com',
+//     subject: `Your Order ${order} Confirmation`,
+//     text: `Thank you for your order! Order ID: ${order}`
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log('Error sending email:', error);
+//     } else {
+//       console.log('Email sent:', info.response);
+//     }
+//   });
+// }
+// //sendEmail("order")
+
+
+let sendEmail = function (req, res) {
+  const order = req.body.order_id;
+
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -21,7 +51,7 @@ function sendEmail(order) {
 
   let mailOptions = {
     from: '24eg204a18@anurag.edu.in',
-    to: 'nowik37917@amgens.com',
+    to: 'vorebe3794@noroasis.com',
     subject: `Your Order ${order} Confirmation`,
     text: `Thank you for your order! Order ID: ${order}`
   };
@@ -29,12 +59,17 @@ function sendEmail(order) {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log('Error sending email:', error);
+      return res.status(500).json({ message: 'Email sending failed', error: error.toString() });
     } else {
       console.log('Email sent:', info.response);
+      return res.status(200).json({ message: 'Email sent successfully', info: info.response });
     }
   });
+};
+
+
+
+module.exports={
+  sendEmail:sendEmail
+  //setDb:setDb
 }
-sendEmail("order")
-
-
-
